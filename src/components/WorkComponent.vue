1052f9d266cb042a65d75712044b15ecfa677572
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-04-24 19:51:27 +0900

    update work

--- src/components/WorkComponent.vue
+++ src/components/WorkComponent.vue
@@ -26,7 +26,7 @@ export default {
         },
         {
           prependAvatar: './public/images/works/imai-logo.png',
-          title: `<br><span class="text-bold lg work-title">今井倫太研究室（2022/2〜現在）</span>`,
+          title: `<br><span class="text-bold lg work-title">今井倫太研究室（2022/2月〜現在）</span>`,
           subtitle: `<span class="text-primary md">研究</span>
                     <br>&mdash; LLMを用いて対話において解釈の齟齬が生じるような聞き逃し発話を特定し、日常対話において応用する研究をしている
                     <br>&mdash; CRESTプロジェクトのオンライン体験会に向けシステムを実装中
@@ -37,15 +37,15 @@ export default {
         { type: 'divider', inset: true },
         {
           prependAvatar: './public/images/works/sdb_logo.png',
-          title: `<br><span class="text-bold lg">ソーシャルデータバンク株式会社（2023/11〜現在）</span>`,
+          title: `<br><span class="text-bold lg">ソーシャルデータバンク株式会社（2023/11月〜現在）</span>`,
           subtitle: `<span class="text-primary md">長期開発インターン</span>
                     <br>&mdash; 会議室予約・出社管理システム「ワクマネ」のフロント・バックエンドの開発をしている（Vue.js, Laravel）
-                    <br>&mdash; 2024/4からRAGを用いたSlackでの質問応答App「ai-manual」のプロジェクトにも参加<br><br>`,
+                    <br>&mdash; 2024/4月からRAGを用いたSlackでの質問応答App「ai-manual」のプロジェクトにも参加<br><br>`,
         },
         { type: 'divider', inset: true },
         {
           prependAvatar: './public/images/works/town-logo.jpg',
-          title: `<br><span class="text-bold lg">TOWN株式会社（2022/8〜2022/9）</span>`,
+          title: `<br><span class="text-bold lg">TOWN株式会社（2022/9月の数日間）</span>`,
           subtitle: `<span class="text-primary md">短期開発インターン</span>
                     <br>&mdash; GoogleMapのAPIを用いて、駐輪場を検索する地図アプリの作成をした（jQuery, CakePHP...）<br><br>`,
         },

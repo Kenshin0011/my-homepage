11f8e0e8a8a1de86b3c99994fac05dd74f7b1c2c
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-04-25 01:51:15 +0900

    modify affiliation

--- src/components/AffiliationComponent.vue
+++ src/components/AffiliationComponent.vue
@@ -1,19 +1,29 @@
 <template>
   <div class="affiliation-container" id="affiliation">
-    <v-list
-        :items="items"
-        lines="five"
-        item-props
-    >
-      <template v-slot:title="{ title }">
-        <div v-html="title"></div>
-      </template>
-      <template v-slot:subtitle="{ subtitle }">
-        <div v-html="subtitle"></div>
-      </template>
+    <div class="text-bold lg">Affiliation</div>
+    <v-list v-for="item in items" :key="item" class="lists">
+      <div class="icon">
+        <img :src="item.prependAvatar" alt="icon" width="60" height="60">
+      </div>
+      <div class="contents">
+        <div class="text-bold lg affiliation-title break">
+          {{ item.titleContent }}
+        </div>
+        <div class="text-primary md">{{ item.subtitle }}</div>
+        <div v-for="(line, index) in item.content" :key="index">
+          <div>&mdash; {{ line.text }}</div>
+        </div>
+        <div v-if="item.conference" class="conference">
+          <div v-for="(line, index) in item.conference" :key="index" class="conference-list">
+            <span v-if="!line.link"> {{ line.text }}</span>
+            <a v-else :href="line.link" class="link">{{ line.text }}</a>
+          </div>
+        </div>
+      </div>
     </v-list>
   </div>
 </template>
+
 
 <script>
 export default {
@@ -21,34 +31,38 @@ export default {
     return {
       items: [
         {
-          title: `<span class="text-bold lg">Affiliation</span>`,
+          prependAvatar: './images/affiliation/imai-logo.png',
+          titleContent: "今井倫太研究室（2023/2月〜現在）",
+          subtitle: "研究",
+          content: [
+            { text: "LLMを用いて対話において解釈の齟齬が生じるような聞き逃し発話を特定し、日常対話において応用する研究をしている" },
+            { text: "CRESTプロジェクトのオンライン体験会に向けシステムを実装中" },
+            { text: "国内学会発表" },
+          ],
+          conference: [
+            { text: "・2024/3/15" },
+            { text: "情報処理学会第86回全国大会", link: "https://www.ipsj.or.jp/event/taikai/86/WEB/data/pdf/2W-01.html" },
+            { text: "・2024/5/30" },
+            { text: "人工知能学会全国大会（第38回）"},
+          ]
         },
         {
-          prependAvatar: './public/images/affiliation/imai-logo.png',
-          title: `<br><span class="text-bold lg affiliation-title">今井倫太研究室（2023/2月〜現在）</span>`,
-          subtitle: `<span class="text-primary md">研究</span>
-                    <br>&mdash; LLMを用いて対話において解釈の齟齬が生じるような聞き逃し発話を特定し、日常対話において応用する研究をしている
-                    <br>&mdash; CRESTプロジェクトのオンライン体験会に向けシステムを実装中
-                    <br>&mdash; 国内学会発表
-                    <br>・2024/3/15 <a href="https://www.ipsj.or.jp/event/taikai/86/WEB/data/pdf/2W-01.html">情報処理学会第86回全国大会</a>
-                    <br>・2024/5/30 人工知能学会全国大会（第38回）<br><br>`,
+          prependAvatar: './images/affiliation/sdb_logo.png',
+          titleContent: "ソーシャルデータバンク株式会社（2023/11月〜現在）",
+          subtitle: "長期開発インターンシップ",
+          content: [
+            { text: "会議室予約・出社管理システム「ワクマネ」のフロント・バックエンドの開発をしている（Vue.js, Laravel）" },
+            { text: "2024/4月からRAGを用いたSlackでの質問応答App「ai-manual」のプロジェクトにも参加" },
+          ],
         },
-        { type: 'divider', inset: true },
         {
-          prependAvatar: './public/images/affiliation/sdb_logo.png',
-          title: `<br><span class="text-bold lg">ソーシャルデータバンク株式会社（2023/11月〜現在）</span>`,
-          subtitle: `<span class="text-primary md">長期開発インターンシップ</span>
-                    <br>&mdash; 会議室予約・出社管理システム「ワクマネ」のフロント・バックエンドの開発をしている（Vue.js, Laravel）
-                    <br>&mdash; 2024/4月からRAGを用いたSlackでの質問応答App「ai-manual」のプロジェクトにも参加<br><br>`,
+          prependAvatar: './images/affiliation/town-logo.jpg',
+          titleContent: "TOWN株式会社（2022/9月の数日間）",
+          subtitle: "短期開発インターンシップ",
+          content: [
+            { text: "GoogleMapのAPIを用いて、駐輪場を検索する地図アプリの作成をした（jQuery, CakePHP...）" },
+          ],
         },
-        { type: 'divider', inset: true },
-        {
-          prependAvatar: './public/images/affiliation/town-logo.jpg',
-          title: `<br><span class="text-bold lg">TOWN株式会社（2022/9月の数日間）</span>`,
-          subtitle: `<span class="text-primary md">短期開発インターンシップ</span>
-                    <br>&mdash; GoogleMapのAPIを用いて、駐輪場を検索する地図アプリの作成をした（jQuery, CakePHP...）<br><br>`,
-        },
-        { type: 'divider', inset: true },
       ],
     }
   },
@@ -57,7 +71,34 @@ export default {
 
 <style scoped>
 .affiliation-container {
-  padding: 90px 16px;
+  padding: 90px 32px;
   background-color: white;
+}
+
+.break {
+  width: 100%; /* 適切な幅の設定 */
+  word-break: break-all;
+  overflow-wrap: break-word;
+}
+
+.lists {
+  display: flex;
+  flex-direction: row;
+  border-bottom: 1px solid var(--vt-c-divider-dark-2);
+}
+
+.icon {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0 20px;
+}
+
+.contents {
+  padding: 24px;
+}
+
+.conference {
+  padding-left: 16px;
 }
 </style>
